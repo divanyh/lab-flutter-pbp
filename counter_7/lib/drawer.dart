0d69f4246a2267bd338740c0aa93d326dd89ea73
Divany <harryndira@gmail.com>
2022-11-17 09:42:26 +0700

    Refactor Drawer

---
+++ counter_7/lib/drawer.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:counter_7/tambah_budget.dart';
+import 'package:counter_7/data_budget.dart';
+import 'package:counter_7/main.dart';
+
+class DrawerSection extends StatelessWidget {
+  const DrawerSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          // Menambahkan clickable menu
+          ListTile(
+            title: const Text('counter_7'),
+            onTap: () {
+              // Route menu ke halaman utama
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const MyHomePage()),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('Tambah Budget'),
+            onTap: () {
+              // Route menu ke halaman form tambah budget
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const MyFormPage()),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('Data Budget'),
+            onTap: () {
+              // Route menu ke halaman data budget
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const MyDataPage()),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
